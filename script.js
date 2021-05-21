b5b5ec98b85ba6e0b91d7cf8ddeb0175e5fa9968
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-05-20 20:19:30 -0700

    I broke the code

--- script.js
+++ script.js
@@ -13,12 +13,13 @@ var numberSeries =
 var specialSeries =
 [ "!" , "@" , "#" , "$" , "%" , "^" ,"&" , "*" , "(" , ")"];
 
-var passwordMax = 8;
 
-var length = "";
 
+
+var criteria ="";
 
 var passwordTest= [];
+
 
 
 function trueRAND() {
@@ -44,20 +45,23 @@ if (chooseOne == 0) {
   console.log("The computer has chosen array 0 " + lowercaseAlphabet[randomLower] +" for the first symbol");
   passwordTest.push( lowercaseAlphabet[randomLower] );
   
+  
 } else if (chooseOne == 1) {
   console.log(randomUpper);
   passwordTest.push( uppercaseAlphabet[randomUpper] );
   console.log("the compuer has chosen array 1 " + uppercaseAlphabet[randomUpper] + " for the first symbol");
   
+  
 } else if (chooseOne == 2) {
   console.log(randomNumber);
   console.log("the computer has chosen array 2 " + numberSeries[randomNumber] + " for the first symbol");
   passwordTest.push( numberSeries[randomNumber] );
-
+  
 } else if (chooseOne == 3) {
   console.log(randomSpecial);
   console.log("the computer has chosen array 4 " + specialSeries[randomSpecial] + " for the first symbol");
   passwordTest.push( specialSeries[randomSpecial] );
+ 
   
 }
 
@@ -72,8 +76,58 @@ return;
 
 
 
+
+
+function firstPrompt() {
+  var length = prompt("Enter the length of your password");
+  
+  if (length <= 123 && length >= 8) {
+    prompt("The password length you chose is " + length+ ".");
+    secondPrompt();
+  } else {
+    prompt("You didn't enter a valid number, press ok");
+    firstPrompt();
+  }
+
+
+  
+}
+
+function secondPrompt() {
+   criteria = prompt("Enter in the first character of your password. Then press ok");
+  prompt("the character you chosen is" + criteria, + "press ok to continue");
+
+  if (typeof criteria === "string") {
+  prompt("The character you chosen is a string, continue by pressing ok");
+  generatePassword();
+
+ 
+  } else if ( Number.isInteger(criteria) === true) {
+    prompt("the character you chosen is a number, continue by pressing ok");
+    generatePassword();
+  
+   
+  } else if (typeof criteria != "number" && typeof criteria != "string") {
+    prompt("the character you chosen is a special, continue by pressing ok"); 
+    generatePassword();
+   
+    
+  }
+
+}
+
+function generatePassword() {
+  var passwordMax = length;
+  while( passwordTest.length <= passwordMax) {
+    trueRAND;
+     }    
+}
+
+
 // Write password to the #password input
 function writePassword() {
+
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -81,46 +135,10 @@ function writePassword() {
 
 }
 
-function firstPrompt() {
-  length = prompt("Enter the length of your password");
-  if (length <= 123) {
-    prompt("the number you chose is " + length);
-    secondPrompt();
-  } else {
-    prompt("You didn't enter a valid number");
-  }
 
-  
-}
-
-function secondPrompt() {
-  var criteria = prompt("Enter in the first character of your password. ");
-  prompt("the character you chosen is" + criteria);
-  
-}
-
-function thirdPrompt() {
-  
-
-}
-
-
-
-function writeThis() {
-
-while( passwordTest.length <= passwordMax) {
-trueRAND();
-}
-var passwordText = document.querySelector("#password");
-passwordText.value = ( passwordTest[0] + passwordTest[1] + passwordTest[2] + passwordTest[3] + passwordTest[4] + passwordTest[5] + passwordTest[6] + passwordTest[7]);
-passwordTest=[];
-}
 
 
 // Add event listener to generate button
-
-
-
 generateBtn.addEventListener("click", firstPrompt);
 
 
