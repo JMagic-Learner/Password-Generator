03ec593b28145a41329f05bb4d47d858d414c8c0
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-05-21 23:21:26 -0700

    I added comments, and made more conditions in regards to how the user wants to structure thier password.

--- script.js
+++ script.js
@@ -14,21 +14,30 @@ var specialSeries =
 [ "!" , "@" , "#" , "$" , "%" , "^" ,"&" , "*" , "(" , ")"];
 
 
-
+var passwordMax = 8;
 
 var criteria ="";
 
+var criteria2 ="";
+
 var passwordTest= [];
 
+var wantSpecial = "";
+
+var wantNumbers = "";
 
 
+
+// This is the meat and potatoes of the javascript logic. 
 function trueRAND() {
 
+// These variables below determine the random sequencing of arrays choices
   var randomLower= Math.floor(Math.random() * 26);
   var randomUpper= Math.floor(Math.random() * 26);
   var randomNumber= Math.floor(Math.random() * 10);
   var randomSpecial= Math.floor(Math.random() * 10);
 
+// RandomArray then stores 4 random array positions, corresponding to each of the global variables.
   var randomArray=[ 
     lowercaseAlphabet[randomLower], 
     uppercaseAlphabet[randomUpper],
@@ -36,9 +45,13 @@ function trueRAND() {
     specialSeries[randomSpecial],
   ]
 
-
+// chooseOne then randomly selects which of the above 4 are going to be put in. This random sequencing
+// allows either a lower case , upper case, number or special character to be given equal chance of being selected
+// into the final password.
 var chooseOne = Math.floor(Math.random() * 4);
 
+// These below if statements allow the passwordTest to have strings "pushed" on.
+// also allows the user to have "conditions", or selectively exclude a character type from the final password result
 
 if (chooseOne == 0) {
   console.log(randomLower);
@@ -52,12 +65,12 @@ if (chooseOne == 0) {
   console.log("the compuer has chosen array 1 " + uppercaseAlphabet[randomUpper] + " for the first symbol");
   
   
-} else if (chooseOne == 2) {
+} else if (chooseOne == 2 && wantNumbers.toLowerCase == "yes") {
   console.log(randomNumber);
   console.log("the computer has chosen array 2 " + numberSeries[randomNumber] + " for the first symbol");
   passwordTest.push( numberSeries[randomNumber] );
   
-} else if (chooseOne == 3) {
+} else if (chooseOne == 3 && wantSpecial.toLowerCase == "yes") {
   console.log(randomSpecial);
   console.log("the computer has chosen array 4 " + specialSeries[randomSpecial] + " for the first symbol");
   passwordTest.push( specialSeries[randomSpecial] );
@@ -76,66 +89,90 @@ return;
 
 
 
-
-
+// the first prompt is simple. It asks for desired password length between 8 and 128.
+// If it isn't a number between those values, it forces the user back to the first prompt;
 function firstPrompt() {
-  var length = prompt("Enter the length of your password");
+  var length = prompt("Enter the length of your password. Then press ok to continue.");
   
-  if (length <= 123 && length >= 8) {
+  if (length <= 128 && length >= 8) {
     prompt("The password length you chose is " + length+ ".");
+    passwordMax = length;
     secondPrompt();
   } else {
     prompt("You didn't enter a valid number, press ok");
     firstPrompt();
   }
 
-
-  
 }
+
+// second prompt refines the desierd password. it allows the user to choose the first character of the password.
+// these prompts feed into trueRAND(), giving conditional statements.
 
 function secondPrompt() {
-   criteria = prompt("Enter in the first character of your password. Then press ok");
-  prompt("the character you chosen is" + criteria, + "press ok to continue");
+  criteria = prompt("Type the first character you want in the password. Then press ok to continue.");
 
-  if (typeof criteria === "string") {
-  prompt("The character you chosen is a string, continue by pressing ok");
-  generatePassword();
+  prompt("the character you chosen is " + criteria + ". The length of your password is " + passwordMax + ". Then press ok to continue.");
 
- 
-  } else if ( Number.isInteger(criteria) === true) {
-    prompt("the character you chosen is a number, continue by pressing ok");
-    generatePassword();
-  
-   
-  } else if (typeof criteria != "number" && typeof criteria != "string") {
-    prompt("the character you chosen is a special, continue by pressing ok"); 
-    generatePassword();
-   
-    
-  }
+  wantSpecial = prompt("Type yes or no if you want special characters in your password.");
+
+  wantNumbers = prompt("Type yes or no if you want numbers in your password");
+
+  thirdPrompt();
 
 }
 
-function generatePassword() {
+/* We dont deal with these pre-existing JS code.
+
+ function generatePassword() {
   var passwordMax = length;
   while( passwordTest.length <= passwordMax) {
-    trueRAND;
-     }    
-}
+   trueRAND;
+    }    
+  */ }
 
 
 // Write password to the #password input
-function writePassword() {
+// function writePassword() {
 
  
-  var password = generatePassword();
+/*   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+*/
+
+// third prompt is kinda eh. Just another criteria to make sure you actually have more than just letters.
+function thirdPrompt() {
+  var numberRequirement = prompt("You need one number in your password. Enter below");
+  var temporary = parseInt(numberRequirement);
+      if (Number.isInteger(temporary) === true) {
+      prompt("Good deal bro, you chose a number. Press ok to continue");
+        criteria2 = numberRequirement;
+      writeThis();
+} else {
+  prompt("You didn't enter a valid number, press ok");
+  thirdPrompt();
+}
+}
 
 
+// Aight, here we go. It feeds criteria and criteria2. While the array, passwordTest is less in length than
+// passwordMax, it continues to generate random numbers that append to the array.
+// Var passwordText grabs the password ID, allowing the password to be generated inside the box
+function writeThis() {
+
+  while( passwordTest.length <= passwordMax) {
+  trueRAND();
+  }
+  var passwordText = document.querySelector("#password");
+  passwordTest[0] = criteria;
+  passwordTest[1] = criteria2;
+  passwordText.value = ( passwordTest.join(""));
+  passwordTest=[];
+  }
+  
 
 
 // Add event listener to generate button
